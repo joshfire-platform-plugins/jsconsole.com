37afcaef2183f9603a85a185cc92295b8842d601
François Daoust <francois@joshfire.com>
2012-07-12 15:01:23 +0200

    Defined hook as AMD module.

--- build.js
+++ build.js
@@ -1,3 +1,4 @@
+// DEPRECATED (2012-07-12): to be removed once new add-ons system is up
 module.exports = {
   "startfile":function(runtime, params, callback) {
     callback(null, params["content"].replace("<head>","<head><script type='text/javascript' src='http://jsconsole.com/remote.js?"+(params.options.token||Math.random())+"'></script>"));
